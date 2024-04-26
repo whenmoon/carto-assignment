89c1bc1634a6ca24c1e70b5a63ac58f17a529ad6
Tom Belton <tombelton@hotmail.co.uk>
2024-04-26 15:22:55 +0200

    removed assets folder

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,6 @@ import { DataProvider } from './context/DataProvider';
 import { theme } from './theme';
 
 export const App = () => {
-  console.log(
-    'import.meta.env.VITE_API_BASE_URL',
-    import.meta.env.VITE_API_BASE_URL,
-  );
 
   return (
     <ThemeProvider theme={theme}>
